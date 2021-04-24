6d460a15adf46d663030c62135508b4454884ebb
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-04-24 15:42:11 +0700

    flask url building

--- hello.py
+++ hello.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/<name>')
-def hello_world(name):
-   return 'hello %s! ' %name
+@app.route('/admin')
+def hello_admin():
+   return 'hello admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'hello  %s' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+  if name == 'admin':
+    return redirect(url_for('hello_admin'))
+  else:
+    return redirect(url_for('hello_guest', guest=name))
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug = True)
