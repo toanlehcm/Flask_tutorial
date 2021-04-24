c45acc5bda62018eafa6ee5c91cfc854ab04f771
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-04-24 15:25:04 +0700

    flask variable rules

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'hello'
+@app.route('/<name>')
+def hello_world(name):
+   return 'hello %s! ' %name
 
 if __name__ == '__main__':
   app.run()
